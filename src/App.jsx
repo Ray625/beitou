81632977b62d59b781c2de48063967f5e552e459
Ray625 <wdvgy32@gmail.com>
2024-05-27 10:45:58 +0800

    feat: add kv component and RWD with min-width: 991px

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import KeyVisionBanner from './components/kv';
+
+function App() {
+  return (
+    <div className="App">
+      <KeyVisionBanner />
+    </div>
+  );
+}
+
+export default App;
