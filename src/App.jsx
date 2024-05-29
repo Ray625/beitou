98faafc6026b33f1a8baaff95d90ea486bf2e759
Ray625 <wdvgy32@gmail.com>
2024-05-29 18:33:00 +0800

    feat: add landscape section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/header';
 import Info from './components/event_Info';
 import Map from './components/event_map';
 import Attraction from './components/event_attraction';
+import Landscape from './components/event_landscape';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Info />
       <Map />
       <Attraction />
+      <Landscape />
     </div>
   );
 }
