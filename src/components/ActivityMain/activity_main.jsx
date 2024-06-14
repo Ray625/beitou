6d17b21cf2501e6a41e2ce959968aec2dbcc416f
Ray625 <wdvgy32@gmail.com>
2024-06-14 18:18:24 +0800

    doc: 更新activity main內容

--- src/components/ActivityMain/activity_main.jsx
+++ src/components/ActivityMain/activity_main.jsx
@@ -2,12 +2,30 @@ import styles from './activity_main.module.scss';
 import { useState, useEffect, useRef } from 'react';
 import { ImgTitle } from '../Ui/titleGroup';
 import { useDevice } from '../../contexts/DeviceContext';
+import { activityList } from './activity_main_info';
 
-// 透過父層傳入的state及setState改變畫面上方tab及下方body內容，body內容還沒確定，所以只有先做上方tab及下方card scroll功能，等內容確定後一樣會依照父層state改變顯示畫面
-const Activity = ({ activity, setActivity }) => {
-  const device = useDevice()
+const Card = ({ color, props }) => {
+  const {title, subtitle, body, icon} = props
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardTitle}>
+        {title}
+        {subtitle && <><br />{subtitle}</>}
+      </div>
+      <div className={styles.hr} style={{ backgroundColor: color }}></div>
+      <div className={styles.cardBody}>{body}</div>
+      <div className={styles.cardIcon} style={{backgroundColor: color}}>
+        <img src={icon} alt="icon" className={styles.iconImg} loading='lazy'/>
+      </div>
+    </div>
+  )
+}
+
+const ScrollCardList = ({ list, color }) => {
   const [leftBtnDisable, setleftBtnDisable] = useState(true)
   const [rightBtnDisable, setRightBtnDisable] = useState(false)
+  const [animationFrameId, setAnimationFrameId] = useState(null);
   const cardRef = useRef(null)
 
   // 畫面下方會有5格card超出畫面，做了用按鈕左右滾動的功能，透過checkIfScrolledToEnd偵測是否已滾動至底部，並改變左右按鈕樣式，此功能只有在PC會顯示，小螢幕則直接滑動
@@ -40,25 +58,116 @@ const Activity = ({ activity, setActivity }) => {
     };
   },[])
 
-  // 點擊按鈕讓cardWapper左右scroll一個卡片width+gap的距離
-  const handleMoveRight = () => {
+  // 按壓移動方式
+
+  const handleMove = (direction) => {
     if (cardRef.current) {
       cardRef.current.scrollBy({
-        left: 264,
-        behavior: 'smooth',
+        left: direction * 18, // Adjust this value to control scroll speed
       });
     }
-  }
+  };
 
-  const handleMoveLeft = () => {
-    if (cardRef.current) {
-      cardRef.current.scrollBy({
-        left: -264,
-        behavior: 'smooth',
-      });
+  const startMove = (direction) => {
+    const move = () => {
+      handleMove(direction);
+      const id = requestAnimationFrame(move);
+      setAnimationFrameId(id);
+    };
+    move();
+  };
+
+  const stopMove = () => {
+    if (animationFrameId) {
+      cancelAnimationFrame(animationFrameId);
+      setAnimationFrameId(null);
     }
-  }
+  };
 
+  return (
+    <>
+    { list.length <= 4 && (
+      <div className={styles.cardContainer} >
+        <div className={styles.cardWrapper} style={{width: '100%'}}>
+          <div className={styles.cardList} >
+            {list.map(card => {
+              return <Card props={card} color={color} key={card.title}/>
+              })}
+          </div>
+        </div>
+      </div>
+    )}
+    { list.length > 4 && (
+      <div className={styles.cardContainer}>
+          <button
+            onMouseDown={() => startMove(-1)}
+            onMouseUp={stopMove}
+            onMouseLeave={stopMove}
+            onTouchStart={() => startMove(-1)}
+            onTouchEnd={stopMove}
+            className={`${styles.btnLeft} ${leftBtnDisable ? styles.disable : ''}`}></button>
+        <div className={styles.cardWrapper} ref={cardRef}>
+          <div className={styles.cardList} >
+            {list.map(card => {
+              return <Card props={card} color={color} key={card.title}/>
+            })}
+          </div>
+        </div>
+          <button
+            onMouseDown={() => startMove(1)}
+            onMouseUp={stopMove}
+            onMouseLeave={stopMove}
+            onTouchStart={() => startMove(1)} 
+            onTouchEnd={stopMove}     
+            className={`${styles.btnRight} ${rightBtnDisable ? styles.disable : ''}`}></button>
+      </div>
+    )}
+  </>
+  )
+}
+
+const ActivityBody = ({ activity, props }) => {
+  const device = useDevice()
+  const {num, color, title, subtitle, icon, date, time, location, describe, cardList } = props
+  
+  return (
+    <div className={`${styles.activityBody} ${num !== activity ? styles.dNone : ''}  `} style={{color: color}}>
+      <div className={styles.titleGroup} style={{ background: `${color}1A` }}>
+        <object data={icon} className={styles.titleicon}> </object>
+        {device === 'mobile' && <h2 className={styles.title}>
+          {title}
+          {subtitle && <><br />{subtitle}</>}
+        </h2>}
+        {device !== 'mobile' && <h2 className={styles.title}>
+          {title}
+          {subtitle && <>・{subtitle}</>}
+        </h2>}
+        <object data={icon} className={styles.titleicon}> </object>
+      </div>
+      <div className={styles.infoGroup}>
+        <div className={styles.info}>
+          <object data='/svg/icon_activity_day.svg' className={styles.icon}> </object>
+          {date}
+        </div>
+        <div className={styles.info}>
+          <object data='/svg/icon_activity_time.svg' className={styles.icon}> </object>
+          {time}
+        </div>
+        <div className={styles.info}>
+          <object data='/svg/icon_activity_pin.svg' className={styles.icon}> </object>
+          {location}
+        </div>
+      </div>
+      <div className={styles.describe}>
+        {describe}
+      </div>
+      <ScrollCardList list={cardList} color={color}/>
+    </div>
+  )  
+}
+
+const Activity = ({ activity, setActivity }) => {
+  
   return (
     <section className={styles.container} >
       <div className={styles.bg}></div>
@@ -76,88 +185,8 @@ const Activity = ({ activity, setActivity }) => {
             <button className={`${styles.tab} ${styles.hover4} ${activity === 4 ? styles.activity4 : ''}`} onClick={()=>setActivity(4)}>親子魔法樂園</button>
             <button className={`${styles.tab} ${styles.hover5} ${activity === 5 ? styles.activity5 : ''}`} onClick={()=>setActivity(5)}>夏日好聲音派對</button>
           </div>
-          <div className={styles.activityBody}>
-            <div className={styles.titleGroup}>
-              <object data='/svg/icon_note.svg' className={styles.titleicon}> </object>
-              {device === 'mobile' && <h2 className={styles.title}>復刻酒家菜<br />音樂同歡唱</h2>}
-              {device !== 'mobile' && <h2 className={styles.title}>復刻酒家菜・音樂同歡唱</h2>}
-              <object data='/svg/icon_note.svg' className={styles.titleicon}> </object>
-            </div>
-            <div className={styles.infoGroup}>
-              <div className={styles.info}>
-                <object data='/svg/icon_activity_day.svg' className={styles.icon}> </object>
-                7/6(六) ~ 7/7(日)
-              </div>
-              <div className={styles.info}>
-                <object data='/svg/icon_activity_time.svg' className={styles.icon}> </object>
-                15:00 ~ 20:00
-              </div>
-              <div className={styles.info}>
-                <object data='/svg/icon_activity_pin.svg' className={styles.icon}> </object>
-                北投七星公園舞台
-              </div>
-            </div>
-            <div className={styles.describe}>
-              邀請北投溫泉飯店及酒家菜業者一起共相盛舉「北投夏日魔法節」現場販售美味酒家菜，還有限量銅板價台鐵酒家菜便當讓民眾品嚐，並邀請專業歌手演出經典那卡西歌曲，讓民眾享受酒家菜一邊聆聽那卡西動人旋律。
-            </div>
-            <div className={styles.cardContainer}>
-              <button onClick={handleMoveLeft} className={`${styles.btnLeft} ${leftBtnDisable ? styles.disable : '' }`}></button>
-              <div className={styles.cardWrapper} ref={cardRef}>
-                <div className={styles.cardList} >
-                  <div className={styles.card}>
-                    <div className={styles.cardTitle}>
-                      那卡西與酒家菜<br/>經典重現
-                    </div>
-                    <div className={styles.hr}></div>
-                    <div className={styles.cardBody}>邀請那卡西表演歌手現場駐唱，重新北投酒家菜經典氛圍</div>
-                    <div className={styles.cardIcon}>
-                      <img src="/icon/tab_2-4.png" alt="icon" className={styles.iconImg} loading='lazy'/>
-                    </div>
-                  </div>
-                  <div className={styles.card}>
-                    <div className={styles.cardTitle}>
-                      那卡西與酒家菜<br/>經典重現
-                    </div>
-                    <div className={styles.hr}></div>
-                    <div className={styles.cardBody}>邀請那卡西表演歌手現場駐唱，重新北投酒家菜經典氛圍</div>
-                    <div className={styles.cardIcon}>
-                      <img src="/icon/tab_2-4.png" alt="icon" className={styles.iconImg} loading='lazy'/>
-                    </div>
-                  </div>
-                  <div className={styles.card}>
-                    <div className={styles.cardTitle}>
-                      那卡西與酒家菜<br/>經典重現
-                    </div>
-                    <div className={styles.hr}></div>
-                    <div className={styles.cardBody}>邀請那卡西表演歌手現場駐唱，重新北投酒家菜經典氛圍</div>
-                    <div className={styles.cardIcon}>
-                      <img src="/icon/tab_2-4.png" alt="icon" className={styles.iconImg} loading='lazy'/>
-                    </div>
-                  </div>
-                  <div className={styles.card}>
-                    <div className={styles.cardTitle}>
-                      那卡西與酒家菜<br/>經典重現
-                    </div>
-                    <div className={styles.hr}></div>
-                    <div className={styles.cardBody}>邀請那卡西表演歌手現場駐唱，重新北投酒家菜經典氛圍</div>
-                    <div className={styles.cardIcon}>
-                      <img src="/icon/tab_2-4.png" alt="icon" className={styles.iconImg} loading='lazy'/>
-                    </div>
-                  </div>
-                  <div className={styles.card}>
-                    <div className={styles.cardTitle}>
-                      那卡西與酒家菜<br/>經典重現
-                    </div>
-                    <div className={styles.hr}></div>
-                    <div className={styles.cardBody}>邀請那卡西表演歌手現場駐唱，重新北投酒家菜經典氛圍</div>
-                    <div className={styles.cardIcon}>
-                      <img src="/icon/tab_2-4.png" alt="icon" className={styles.iconImg} loading='lazy'/>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <button onClick={handleMoveRight} className={`${styles.btnRight} ${rightBtnDisable ? styles.disable : ''}`}></button>
-            </div>
+          <div className={styles.bodyContainer}>
+            {activityList.map(item => <ActivityBody activity={activity} props={ item } key={activity.num}/> )}
           </div>
         </div>
       </div>
